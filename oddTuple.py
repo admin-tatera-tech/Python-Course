5bafd83c09eb0f1daca829c5759b7be1b39c702a
admin-tatera-tech <86271401+admin-tatera-tech@users.noreply.github.com>
2021-06-26 20:14:34 -0500

    Update oddTuple.py

--- oddTuple.py
+++ oddTuple.py
@@ -4,12 +4,22 @@ def oddTuples(aTup):
     
     returns: tuple, every other element of aTup. 
     '''
+    #new_tuple is a blank tuple to store values in
     new_tuple = ()
-
+    
+    #Creates a new for loop to interate of tuple aTup
     for i in aTup:
+        
+        #Checks the index value of i to see if it is odd or even
         if aTup.index(i) == 0 or aTup.index(i) % 2 == 0:
+            
+            #appends i to new_tuple
             new_tuple = new_tuple + (i,)
+            
+            #for debugging i
             print(aTup.index(i))
+            
+    #returns tuple new_tuple        
     return new_tuple
 
 oddTuples((17, 9, 1, 0, 13, 8, 0, 20, 4, 17))
