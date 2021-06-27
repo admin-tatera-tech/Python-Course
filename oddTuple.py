34ea89354c661d8ee356849fe0ba05e315827327
admin-tatera-tech <86271401+admin-tatera-tech@users.noreply.github.com>
2021-06-26 20:58:04 -0500

    Update oddTuple.py

--- oddTuple.py
+++ oddTuple.py
@@ -10,7 +10,11 @@ def oddTuples(aTup):
     #Creates a new for loop to interate of tuple aTup
     for i in aTup:
         
-        #Checks the index value of i to see if it is odd or even
+        '''
+        Checks the index value of i to see if it is odd or even
+        however it is not checking the index and cutting some numbers 
+        out or returning ones that shouldnt be returned
+        '''
         if aTup.index(i) == 0 or aTup.index(i) % 2 == 0:
             
             #appends i to new_tuple
