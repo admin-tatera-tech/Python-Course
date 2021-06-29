0f388836a47069da4da34a062adb78e9c064187d
admin-tatera-tech <86271401+admin-tatera-tech@users.noreply.github.com>
2021-06-29 04:56:34 -0500

    Update getavailableworking on.py

--- getavailableworking on.py
+++ getavailableworking on.py
@@ -8,7 +8,8 @@ def getAvailableLetters(lettersGuessed):
        
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+        #Need to return alphabet if the list of lettersGuessed is empty, but not sure how
         return ''.join(sorted(list.append(set(list(alphabet)).difference(set(lettersGuessed)))))
     
-    
+# works when player has guessed a letter but not when list is empty  
 print(getAvailableLetters(['e', 'i', 'k', 'p', 'r', 's']))
